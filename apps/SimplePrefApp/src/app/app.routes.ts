40904a9e323c8e3949ccefe73230f7e38533cda2
aashraf2510 <aashraf.elevatetech@gmail.com>
2025-06-18 00:30:21 +0300

    Add design + tailwind

--- apps/SimplePrefApp/src/app/app.routes.ts
+++ apps/SimplePrefApp/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./core/pages/home.component').then((c) => c.HomeComponent),
+  },
+];
